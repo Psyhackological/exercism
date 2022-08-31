bdc2b024aba5b790f291b84dddd9d0b8e156a3e0
Psyhackological <konradkon@duck.com>
2022-08-31 23:08:02 +0200

    Added solution to magazine-cutout

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,28 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut magazine_hashmap = HashMap::new();
+    let mut note_hashmap = HashMap::new();
+
+    // Thanks Insomnia#7030
+    for m in magazine {
+        for word in m.split_whitespace() {
+            magazine_hashmap
+                .entry(word)
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+        }
+    }
+
+    for word in note.iter().flat_map(|s| s.split_whitespace()) {
+        note_hashmap
+            .entry(word)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+
+    // Thanks skeletizzle#8654
+    note_hashmap
+        .iter()
+        .all(|(word, count)| Some(count) <= magazine_hashmap.get(word))
 }
