4ac759a3866435a731179d19f724e1b4ffaecf9e
Psyhackological <konradkon@duck.com>
2023-07-08 23:43:00 +0200

    Updated rust armstrong-numbers, so overflow works correctly

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,13 +1,21 @@
-// "hacky solution"
+// kind-of-iterator solution
 pub fn is_armstrong_number(num: u32) -> bool {
     let str_num: String = num.to_string();
     let num_length: usize = str_num.len();
 
-    let armstrong_sum = str_num
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .map(|d| d.pow(num_length as u32))
-        .sum();
+    let mut armstrong_sum: u32 = 0;
+
+    for digit in str_num.chars().map(|c| c.to_digit(10).unwrap()) {
+        match digit.checked_pow(num_length as u32) {
+            Some(val) => {
+                armstrong_sum = match armstrong_sum.checked_add(val) {
+                    Some(new_sum) => new_sum,
+                    None => return false,
+                }
+            }
+            None => return false,
+        };
+    }
 
     num == armstrong_sum
 }
--- rust/armstrong-numbers/src/lib2.rs
+++ rust/armstrong-numbers/src/lib2.rs
@@ -1,13 +1,24 @@
 // "hacky solution"
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut armstrong_sum: u32 = 0;
+    let mut armstrong_sum: Option<u32> = Some(0);
     let str_num: String = num.to_string();
     let num_length: usize = str_num.len();
 
     for digit_char in str_num.chars() {
+        let digit = digit_char as u32 - 48;
         // https://stackoverflow.com/a/41380607
-        armstrong_sum += (digit_char as u32 - 48).pow(num_length as u32);
+        let powered = match digit.checked_pow(num_length as u32) {
+            Some(result) => result,
+            None => return false, // Overflow occurred in power operation
+        };
+        armstrong_sum = match armstrong_sum.unwrap().checked_add(powered) {
+            Some(result) => Some(result),
+            None => return false, // Overflow occurred in addition operation
+        };
     }
 
-    num == armstrong_sum
+    match armstrong_sum {
+        Some(sum) => num == sum,
+        None => false, // Overflow occurred
+    }
 }
