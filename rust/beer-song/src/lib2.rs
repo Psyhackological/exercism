5c90827967bddb94241d1b84608b295177967676
Psyhackological <konradkon@duck.com>
2023-07-08 23:54:17 +0200

    Updated all of the beer-song  files

--- rust/beer-song/src/lib2.rs
+++ rust/beer-song/src/lib2.rs
@@ -3,8 +3,8 @@ pub fn verse(n: u32) -> String {
     // However that would mean u32 could be negative
     match n {
         2 => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n", n ),
-        1 => format!("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"), 
-        0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(), 
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         _ => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n", n, (n - 1)),
     }
 }
@@ -13,7 +13,7 @@ pub fn sing(start: u32, end: u32) -> String {
     let mut beer_song = String::new();
     for n in (end..=start).rev() {
         beer_song.push_str(&verse(n));
-        beer_song.push_str("\n");
+        beer_song.push('\n');
     }
 
     // remove last \n character
