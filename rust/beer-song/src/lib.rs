2399747753c22b74e260a7f8d29931d1cdbe3f06
Psyhackological <konradkon@duck.com>
2023-07-08 20:05:12 +0200

    beer-song clippy suggestion "warning: useless use of `format!`"

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -3,8 +3,8 @@ pub fn verse(n: u32) -> String {
     // However that would mean u32 could be negative
     match n {
         2 => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n", n ),
-        1 => format!("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"), 
-        0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(), 
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         _ => format!("{0} bottles of beer on the wall, {0} bottles of beer.\nTake one down and pass it around, {1} bottles of beer on the wall.\n", n, (n - 1)),
     }
 }
