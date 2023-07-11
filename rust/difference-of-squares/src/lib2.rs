fd0bac8746c6b51592fcb2c26de33559968e8f20
Psyhackological <konradkon@duck.com>
2023-07-11 18:07:26 +0200

    Added rust difference-of-squares solution

---
+++ rust/difference-of-squares/src/lib2.rs
@@ -0,0 +1,14 @@
+// Second "readable" solution.
+pub fn square_of_sum(n: u32) -> u32 {
+    (1..=n).fold(0, |acc, item| acc + item).pow(2)
+    // clippy suggests instead
+    // (1..=n).sum::<u32>().pow(2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).fold(0, |acc, item| acc + item.pow(2))
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
