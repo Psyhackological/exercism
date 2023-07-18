40466bd6e8402e375c47ac33c3ae18d7582af7a0
Psyhackological <konradkon@duck.com>
2023-07-18 22:26:00 +0200

    Add final version of rust space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,8 +1,19 @@
 const EARTH_YEAR_IN_SECONDS: f64 = 31557600.0;
 
-// 3. Consider using a macro to avoid repetition in trait implementation:
+pub struct Duration(f64);
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self(s as f64)
+    }
+}
+
+pub trait Planet {
+    fn years_during(d: &Duration) -> f64;
+}
+
 macro_rules! define_and_implement_planet {
-    ($planet_name:ident, $orbital_period:expr) => {
+    ($planet_name:ident, $orbital_period:literal) => {
         pub struct $planet_name;
 
         impl Planet for $planet_name {
@@ -13,8 +24,6 @@ macro_rules! define_and_implement_planet {
     };
 }
 
-// 1. Understand the Problem: Convert an age in seconds into years on different planets.
-// Like this math
 define_and_implement_planet!(Mercury, 0.2408467);
 define_and_implement_planet!(Venus, 0.61519726);
 define_and_implement_planet!(Earth, 1.0);
@@ -23,17 +32,3 @@ define_and_implement_planet!(Jupiter, 11.862615);
 define_and_implement_planet!(Saturn, 29.447498);
 define_and_implement_planet!(Uranus, 84.016846);
 define_and_implement_planet!(Neptune, 164.79132);
-
-#[derive(Debug)]
-pub struct Duration(f64);
-
-impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        Self(s as f64)
-    }
-}
-
-// 2. Define a Trait `Planet`:
-pub trait Planet {
-    fn years_during(d: &Duration) -> f64;
-}
