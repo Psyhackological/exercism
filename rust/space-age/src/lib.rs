9e5dd5f610e862e1383b521f6ae49eb8d699a7cd
Psyhackological <konradkon@duck.com>
2023-07-18 21:29:17 +0200

    Small improvments for rust space-age 1st solution

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,11 +1,13 @@
 const EARTH_YEAR_IN_SECONDS: f64 = 31557600.0;
 
-// 4. Consider using a macro to avoid repetition in trait implementation:
-macro_rules! implement_planet {
-    ($planet:ident, $orbital_period:expr) => {
-        impl Planet for $planet {
+// 3. Consider using a macro to avoid repetition in trait implementation:
+macro_rules! define_and_implement_planet {
+    ($planet_name:ident, $orbital_period:expr) => {
+        pub struct $planet_name;
+
+        impl Planet for $planet_name {
             fn years_during(d: &Duration) -> f64 {
-                d.0 as f64 / ($orbital_period * EARTH_YEAR_IN_SECONDS)
+                d.0 / ($orbital_period * EARTH_YEAR_IN_SECONDS)
             }
         }
     };
@@ -13,21 +15,21 @@ macro_rules! implement_planet {
 
 // 1. Understand the Problem: Convert an age in seconds into years on different planets.
 // Like this math
-implement_planet!(Mercury, 0.2408467);
-implement_planet!(Venus, 0.61519726);
-implement_planet!(Earth, 1.0);
-implement_planet!(Mars, 1.8808158);
-implement_planet!(Jupiter, 11.862615);
-implement_planet!(Saturn, 29.447498);
-implement_planet!(Uranus, 84.016846);
-implement_planet!(Neptune, 164.79132);
+define_and_implement_planet!(Mercury, 0.2408467);
+define_and_implement_planet!(Venus, 0.61519726);
+define_and_implement_planet!(Earth, 1.0);
+define_and_implement_planet!(Mars, 1.8808158);
+define_and_implement_planet!(Jupiter, 11.862615);
+define_and_implement_planet!(Saturn, 29.447498);
+define_and_implement_planet!(Uranus, 84.016846);
+define_and_implement_planet!(Neptune, 164.79132);
 
 #[derive(Debug)]
-pub struct Duration(u64);
+pub struct Duration(f64);
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        Self(s)
+        Self(s as f64)
     }
 }
 
@@ -35,14 +37,3 @@ impl From<u64> for Duration {
 pub trait Planet {
     fn years_during(d: &Duration) -> f64;
 }
-
-// 3. Create structs for each planet. e.g.:
-// This was already created so I leave it as it is
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
