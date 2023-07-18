78421d64b6a3631a871b6b935c1add739f2e6485
Psyhackological <konradkon@duck.com>
2023-07-18 22:32:38 +0200

    Add bobahop's solution for space-age

---
+++ rust/space-age/src/lib2.rs
@@ -0,0 +1,44 @@
+// bobahop's solution: https://exercism.org/tracks/rust/exercises/space-age/solutions/bobahop
+// For archiving purposes
+#[derive(Debug)]
+pub struct Duration {
+    pub value: f64,
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self { value: s as f64 }
+    }
+}
+
+pub trait Planet {
+    const EARTH_YEAR_SECONDS: f64 = 31557600.0;
+    const EARTH_YEAR_RATIO: f64;
+
+    fn years_during(d: &Duration) -> f64 {
+        d.value / Self::EARTH_YEAR_SECONDS / Self::EARTH_YEAR_RATIO
+    }
+}
+
+macro_rules! boilerplate {
+    ($($t:ident => $e:expr),+) => {
+        $(
+            pub struct $t {}
+
+            impl Planet for $t {
+                const EARTH_YEAR_RATIO: f64 = $e;
+            }
+        )*
+    }
+}
+
+boilerplate!(
+    Mercury => 0.2408467,
+    Venus => 0.61519726,
+    Earth => 1.0,
+    Mars => 1.8808158,
+    Jupiter => 11.862615,
+    Saturn => 29.447498,
+    Uranus => 84.016846,
+    Neptune => 164.79132
+);
