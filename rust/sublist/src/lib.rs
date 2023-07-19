c298de01479e216f44933a7e63828acee516063d
Psyhackological <konradkon@duck.com>
2023-07-19 20:55:41 +0200

    Add rust sublist solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,6 +6,15 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    let is_sublist =
+        |a: &[T], b: &[T]| a.is_empty() || b.windows(a.len()).any(|window| window == a);
+
+    use Comparison::*;
+    match (first_list, second_list) {
+        (a, b) if a == b => Equal,
+        (a, b) if is_sublist(a, b) => Sublist,
+        (a, b) if is_sublist(b, a) => Superlist,
+        _ => Unequal,
+    }
 }
