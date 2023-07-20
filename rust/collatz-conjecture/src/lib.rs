83be6c76ce17f0cbaf5cd789d5d2115cfa7f562a
Psyhackological <konradkon@duck.com>
2023-07-20 16:27:31 +0200

    Add rust collatz-conjecture solution

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,5 +1,28 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+    match n {
+        0 => None,
+        1 => Some(0),
+        _ => {
+            let mut n = n;
+            let mut steps = 0_u64;
+            while n != 1 {
+                match n % 2 {
+                    0 => {
+                        steps += 1;
+                        n /= 2;
+                    }
+                    1 => {
+                        // reverse n * 3 + 1 -> (MAX_VALUE - 1) / 3
+                        if n > (u64::MAX - 1) / 3 {
+                            return None;
+                        }
+                        steps += 1;
+                        n = n * 3 + 1;
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            Some(steps)
+        }
+    }
 }
