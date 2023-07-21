b914f6fa42c50be22da5838ce4933f76a135f8ad
Psyhackological <konradkon@duck.com>
2023-07-21 16:45:25 +0200

    Add rust matching-brackets solution

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,1 +1,26 @@
-pub fn brackets_are_balanced(string: &str) -> bool {}
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut brackets_vec: Vec<char> = Vec::with_capacity(string.len());
+
+    for character in string.chars() {
+        match character {
+            '(' | '[' | '{' => brackets_vec.push(character),
+            ')' => {
+                if brackets_vec.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if brackets_vec.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if brackets_vec.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => continue,
+        }
+    }
+    brackets_vec.is_empty()
+}
