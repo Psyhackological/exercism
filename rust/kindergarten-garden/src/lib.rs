d0ab3f47ce22818df5a78873dfe8ffdae2077a8b
Psyhackological <konradkon@duck.com>
2023-07-24 23:03:31 +0200

    Add 2nd solution to rust kingergarten-garden

--- rust/kindergarten-garden/src/lib.rs
+++ rust/kindergarten-garden/src/lib.rs
@@ -1,41 +1,30 @@
-// 1st solution
-pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let digram_lines = diagram.split('\n').collect::<Vec<&str>>();
+// 2nd solution
+// Thanks to:
+// https://exercism.org/tracks/rust/exercises/kindergarten-garden/solutions/Tyraan
+// https://exercism.org/tracks/rust/exercises/kindergarten-garden/solutions/sezoka
 
-    let start: usize = match student {
-        "Alice" => 0,
-        "Bob" => 2,
-        "Charlie" => 4,
-        "David" => 6,
-        "Eve" => 8,
-        "Fred" => 10,
-        "Ginny" => 12,
-        "Harriet" => 14,
-        "Ileana" => 16,
-        "Joseph" => 18,
-        "Kincaid" => 20,
-        "Larry" => 22,
-        _ => panic!("Unknown student"),
+const CHILDREN: [&str; 12] = [
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana", "Joseph",
+    "Kincaid", "Larry",
+];
+
+pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
+    let cup_start = CHILDREN.iter().position(|&s| s == student).unwrap_or(0) * 2;
+
+    let char_map = |c: char| match c {
+        'V' => "violets",
+        'R' => "radishes",
+        'C' => "clover",
+        'G' => "grass",
+        _ => "unknown",
     };
 
-    let mut cups: Vec<&'static str> = Vec::new();
-
-    for line in digram_lines {
-        let two_plants: Vec<&str> = line
-            .chars()
-            .skip(start)
-            .take(2)
-            .map(|c| match c {
-                'V' => "violets",
-                'R' => "radishes",
-                'C' => "clover",
-                'G' => "grass",
-                _ => "unknown",
-            })
-            .collect();
-
-        cups.extend(two_plants);
-    }
-
-    cups
+    diagram
+        .lines()
+        .flat_map(|line| {
+            line[cup_start..=cup_start + 1]
+                .chars()
+                .map(|cup| char_map(cup))
+        })
+        .collect()
 }
