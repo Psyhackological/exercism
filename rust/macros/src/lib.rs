d05488644db6a0f31a5b6712b54acb44787204b5
Psyhackological <konradkon@duck.com>
2024-03-02 17:37:54 +0100

    Add macros rust solution

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,4 +1,19 @@
 #[macro_export]
 macro_rules! hashmap {
+    // Empty hashmap case
+    () => {
+        ::std::collections::HashMap::new()
+    };
 
+    // Hashmap with one or more key-value pairs
+    (
+        $($key:expr => $value:expr),+ // Match one or more key-value pairs
+        $(,)?                         // Optionally match a trailing comma
+    ) => {{ // First { is macro block, second {  is expression start
+        let mut hashmap = ::std::collections::HashMap::new(); // Create a new hashmap
+        $(
+            hashmap.insert($key, $value); // Insert each key-value pair into the hashmap
+        )+
+        hashmap // Return the populated hashmap
+    }}; // First } is expression end, second }; is macro end
 }
