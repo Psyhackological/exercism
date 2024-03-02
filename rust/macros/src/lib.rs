477f2697215bef157609b956d75ac63d269cc92c
Psyhackological <konradkon@duck.com>
2024-03-02 17:26:39 +0100

    Clean macro lib.rs so body is empty

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,23 +1,4 @@
-use std::collections::HashMap;
-
 #[macro_export]
 macro_rules! hashmap {
-    () => HashMap::new();
-    ($a: expr, $b: expr) => HashMap::new();
-    () => {
-        let mut hm = HashMap::new();
-    };
-}
 
-// Dowolny klucz
-// Dowolna wartosc
-// Strzalka oddzielajaca
-// Druugie to expression
-// hashmap!('a' => 3, 'b' => 11, 'z' => 32)
-// {
-//    let mut hm = HashMap::new();
-//    hm.insert('a', 3);
-//    hm.insert('b', 11);
-//    hm.insert('z', 32);
-//    hm
-// }
+}
