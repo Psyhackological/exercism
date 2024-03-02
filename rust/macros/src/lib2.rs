3737908f2913e3e79ba6ceac31072b3a4d8044fc
Psyhackological <konradkon@duck.com>
2024-03-02 17:53:42 +0100

    Add rust macro solution small preallocated improvement for HashMap

--- rust/macros/src/lib2.rs
+++ rust/macros/src/lib2.rs
@@ -10,8 +10,7 @@ macro_rules! hashmap {
         $($key:expr => $value:expr),+ // Match one or more key-value pairs
         $(,)?                         // Optionally match a trailing comma
     ) => {{ // First { is macro block, second {  is expression start
-        let count = [$( { let _ = &$key; 1 } ),+].len(); // Calculates key count
-        let mut hashmap = ::std::collections::HashMap::with_capacity(count); // Create a new preallocated hashmap
+        let mut hashmap = ::std::collections::HashMap::new(); // Create a new hashmap
         $(
             hashmap.insert($key, $value); // Insert each key-value pair into the hashmap
         )+
