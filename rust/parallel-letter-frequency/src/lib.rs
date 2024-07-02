3374951e07649ae688559a61a01527a0e901a642
Psyhackological <konradkon@duck.com>
2024-07-02 22:02:07 +0200

    First iteration of parallel-letter-frequency

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,13 +1,20 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
-        }
-    );
+    let mut char_count = HashMap::new();
+
+    input
+        .iter() // we want iterate over &[&str]
+        .flat_map(|l| l.split_whitespace()) // we flatten it, create iterator by whitespace
+        .for_each(|w| {
+            // now we have a word
+            w.to_lowercase()
+                .chars()
+                .filter(|c| c.is_alphabetic())
+                .for_each(|ch| {
+                    *char_count.entry(ch).or_insert(0) += 1;
+                });
+        });
+
+    char_count
 }
